c65d707d2ab89a47734bf11947dcc5ad5ae5cb24
jehosubedi <jehosubedi4@gmail.com>
2025-10-10 15:52:43 +0800

    added changes to codebase

--- graduate_attributes/hooks.py
+++ graduate_attributes/hooks.py
@@ -11,15 +11,15 @@ app_license = "mit"
 # required_apps = []
 
 # Each item in the list will be shown as an app in the apps page
-# add_to_apps_screen = [
-# 	{
-# 		"name": "graduate_attributes",
-# 		"logo": "/assets/graduate_attributes/logo.png",
-# 		"title": "APC GA",
-# 		"route": "/graduate_attributes",
-# 		"has_permission": "graduate_attributes.api.permission.has_app_permission"
-# 	}
-# ]
+add_to_apps_screen = [
+	{
+		"name": "graduate_attributes",
+		"logo": "/assets/graduate_attributes/logo.png",
+		"title": "APC GA",
+		"route": "/graduate_attributes",
+		# "has_permission": "graduate_attributes.api.permission.has_app_permission"
+	}
+]
 
 # Includes in <head>
 # ------------------
