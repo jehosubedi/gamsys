e498167e11d9b011efbf55cd2b30f8d9a04e5da5
jehosubedi <jehosubedi4@gmail.com>
2025-10-19 20:14:28 +0800

    ui: updated self assessment doctypes and fixed bugs

---
+++ graduate_attributes/apc_ga/doctype/year_level/test_year_level.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2025, Team Tacos and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests import IntegrationTestCase
+
+
+# On IntegrationTestCase, the doctype test records and all
+# link-field test record dependencies are recursively loaded
+# Use these module variables to add/remove to/from that list
+EXTRA_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
+IGNORE_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
+
+
+
+<<<<<<< HEAD:graduate_attributes/apc_ga/doctype/self_assessment/test_self_assessment.py
+class IntegrationTestSelfAssessment(IntegrationTestCase):
+=======
+class IntegrationTestYearLevel(IntegrationTestCase):
+>>>>>>> 223fcdc (ui: updated self assessment doctypes and fixed bugs):graduate_attributes/apc_ga/doctype/year_level/test_year_level.py
+	"""
+	Integration tests for YearLevel.
+	Use this class for testing interactions between multiple components.
+	"""
+
+	pass
