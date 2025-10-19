a1a0ed9c164ba693cc28e969956d8eb1bb610e51
jehosubedi <jehosubedi4@gmail.com>
2025-10-19 08:57:03 +0800

    ui: added new changes to workspace

--- graduate_attributes/apc_ga/doctype/at_risk_alert/at_risk_alert.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, Team Tacos and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("At Risk Alert", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- graduate_attributes/apc_ga/doctype/program/program.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, Team Tacos and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Program", {
-// 	refresh(frm) {
-
-// 	},
-// });
