ee5271ba596571d25fd431e75969a01dadc20d21
jehosubedi <jehosubedi4@gmail.com>
2025-10-21 12:49:17 +0800

    ui: fixed workspaces

--- graduate_attributes/apc_ga/doctype/attributes/test_attributes.py
+++ graduate_attributes/apc_ga/doctype/attributes/test_attributes.py
@@ -4,6 +4,7 @@
 # import frappe
 from frappe.tests import IntegrationTestCase
 
+
 # On IntegrationTestCase, the doctype test records and all
 # link-field test record dependencies are recursively loaded
 # Use these module variables to add/remove to/from that list
@@ -11,9 +12,10 @@ EXTRA_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
 IGNORE_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
 
 
-class IntegrationTestProgramList(IntegrationTestCase):
+
+class IntegrationTestAttributes(IntegrationTestCase):
 	"""
-	Integration tests for ProgramList.
+	Integration tests for Attributes.
 	Use this class for testing interactions between multiple components.
 	"""
 
