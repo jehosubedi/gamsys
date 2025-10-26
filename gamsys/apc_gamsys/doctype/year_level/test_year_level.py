3396073110e5e465560d9644bc4c9b9a3ceb71bb
jehosubedi <jehosubedi4@gmail.com>
2025-10-26 21:19:36 +0800

    ui: added new changes to doctypes

---
+++ gamsys/apc_gamsys/doctype/year_level/test_year_level.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, teamtacos and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestYearLevel(FrappeTestCase):
+	pass
